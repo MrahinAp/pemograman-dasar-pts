bd5fff16acbc7addb633b27060858ff10738acd2
MrahinAp <111964792+MrahinAp@users.noreply.github.com>
2022-09-28 16:44:47 +0700

    Add files via upload

---
+++ M.Rahin 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{  
+    cout << "Nama : Muhamad Rahin Almassyah Putra" << endl;
+    cout << "Kelas : X RPL 1" << endl;
+    cout << "Nilai: 95 "<< endl;
+    
+ 
+}
+
